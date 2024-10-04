ba9d77cac6ec6215650c40614e34e68b7c9be602
CoolestMegan <mr0133@uah.edu>
2024-10-03 23:16:13 -0500

    Shooting WIP
    I register an enemy is in range. I say "SHOOT!" but that's it so far

--- TowerDefense/Assets/EnemyAttack.cs
+++ TowerDefense/Assets/EnemyAttack.cs
@@ -8,16 +8,52 @@ public class EnemyAttack : MonoBehaviour
     public Transform target;
     public float range = 15f;
 
+    public string enemyTag = "Enemy";
+
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+    
+    void UpdateTarget()
+    {
+        GameObject enemies = GameObject.FindGameObjectWithTag(enemyTag);
+        float distanceToEnemy = Vector3.Distance(transform.position, enemies.transform.position);
+        if(distanceToEnemy <= range)
+        {
+            target = enemies.transform;
+        }
+        else
+        {
+            target = null;
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(target == null)
+        {
+            return;
+        }
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        Debug.Log("SHOOT!");
     }
 
     void OnDrawGizmosSelected()
